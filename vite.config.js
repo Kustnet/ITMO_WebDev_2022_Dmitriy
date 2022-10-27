3c155d8459c1ba03252e762b3edc12c56d06d6dd
Dmitriy <Kust-net@mail.ru>
2022-10-27 21:58:55 +0300

    Lesson 14

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 // vite.config.js
+import path from 'path';
+
 export default {
-    // config options
-    server:{
-        port:8888
-    }
-}
+  // config options
+  resolve: {
+      alias: {
+          '@/': `${path.resolve(__dirname, 'src')}/`,
+      },
+  },
+  server: {
+    port: 8888,
+  },
+};
